36fb1617aa55a28ec246a1d4599b8f0345ce08d0
Rafael Garcia <rafael.garcia@clever.com>
2015-03-24 20:51:43 -0700

    LICENSE.md -> LICENSE

--- examples/add-license/add_license.sh
+++ examples/add-license/add_license.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if cmp -s "LICENSE.md" "$1/LICENSE.md"
+if cmp -s "LICENSE" "$1/LICENSE"
 then
    echo "not adding a license"
    exit 1
 else
    echo "Adding a license"
-   cp "LICENSE.md" "$1/LICENSE.md"
+   cp "LICENSE" "$1/LICENSE"
    exit 0
 fi
